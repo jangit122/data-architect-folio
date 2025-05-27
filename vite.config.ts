a9e2888edda6bbda622d6244397066136799e699
jangit122 <janardhan.gurram9@gmail.com>
2025-05-27 22:52:55 +0530

    Add redirect for client-side routing2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "data-architect-folio",
+  base: "/data-architect-folio/",
   server: {
     host: "::",
     port: 8080,
